e2905bb2f0188f8cd1f3222e193806512aba4d35
Anastasia Korshunova <gatta.gizmo@gmail.com>
2024-11-04 12:56:20 +0200

    could be better

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -1,10 +1,10 @@
-export function createCard(template, element, deleteCard, handleLike, handleImageClick) {
+export function createCard(template, cardInfo, deleteCard, handleLike, handleImageClick) {
     const cardElement = template.content.querySelector('.places__item').cloneNode(true);
     const cardImage = cardElement.querySelector('.card__image');
 
-    cardImage.setAttribute('src', element.link);
-    cardImage.setAttribute('alt', element.name);
-    cardElement.querySelector('.card__title').textContent = element.name;
+    cardImage.setAttribute('src', cardInfo.link);
+    cardImage.setAttribute('alt', cardInfo.name);
+    cardElement.querySelector('.card__title').textContent = cardInfo.name;
 
     const deleteButton = cardElement.querySelector('.card__delete-button');
     deleteButton.addEventListener("click", (evt) => deleteCard(evt, cardElement));
@@ -12,7 +12,7 @@ export function createCard(template, element, deleteCard, handleLike, handleImag
     const likeButton = cardElement.querySelector('.card__like-button');
     likeButton.addEventListener('click', handleLike);
 
-    cardImage.addEventListener('click', () => handleImageClick(element));
+    cardImage.addEventListener('click', () => handleImageClick(cardInfo));
 
     return cardElement;
 }
